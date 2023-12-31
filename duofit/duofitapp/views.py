fe49c2caf8df26a1db1b242fa58209c9268b8919
Toni Iglesias Carrión <t.iglesias@ionnavis.com>
2023-12-31 11:51:39 +0100

    funcionamiento basico

--- duofit/duofitapp/views.py
+++ duofit/duofitapp/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
@@ -9,6 +11,8 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
+from datetime import datetime
+
 def index(request):
     if not request.user.is_authenticated:
         return redirect('login')
@@ -17,12 +21,18 @@ def index(request):
     exercice_config = ExerciceConfig.objects.filter(id_user=user_id).first()
 
     if not exercice_config:
-        return redirect('settings')  # Redirige a la página de configuración si no hay configuración existente
+        return redirect('settings')
 
-    # Obtén el número de entrenamientos completados hoy
-    today_completed_trainings = exercice_config.get_today_completed_trainings()
+    # Actualizar streak al abrir la página
+    refresh_streak(user_id)
 
-    return render(request, 'index.html', {'today_completed_trainings': today_completed_trainings})
+    return render(request, 'index.html', {
+        'exercice_config': exercice_config,
+        'today_completed_trainings': exercice_config.get_today_completed_trainings(),
+        'today_goal': exercice_config.get_today_goal(),
+        'weekly_completed_trainings': exercice_config.get_weekly_trainings(),
+    })
+
     
 def login_view(request):
     if request.method == 'POST':
@@ -73,10 +83,24 @@ def log_training(request):
         # Registra un nuevo entrenamiento en la fecha actual
         if exercice_config:
             ExerciceLog.objects.create(user=user, date=timezone.now())
+            ExerciceConfig.objects.get(id_user=user.id).add_streak()
+            #ExerciceConfig.objects.get(id_user=user.id).log_training_date()
 
         return redirect('index')  # Redirige de nuevo a la página principal
 
     return redirect('index')
 
 
+#Utility functions
 
+def refresh_streak(user_id):
+    exercice_config = ExerciceConfig.objects.get(id_user=user_id)
+    today_goal = exercice_config.get_today_goal()
+    today_completed_trainings = exercice_config.get_today_completed_trainings()
+
+    if today_goal <= 0 and exercice_config.streak >= 1:
+        # Verificar que hoy no haya entrenamientos
+        if today_completed_trainings == 0:
+            exercice_config.streak += 1
+            exercice_config.last_training_date = timezone.now()
+            exercice_config.save()
