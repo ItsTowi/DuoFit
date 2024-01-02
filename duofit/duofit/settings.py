e1dd290177e824d0775fe2821496b4d13039c2c2
Toni Iglesias Carrión <t.iglesias@ionnavis.com>
2024-01-02 12:15:48 +0100

    gitignore .env

--- duofit/duofit/settings.py
+++ duofit/duofit/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
