057184f6b1ef8ff736f336c1eef1407b4730bc78
Toni Iglesias Carrión <t.iglesias@ionnavis.com>
2024-08-05 08:09:55 +0200

    commit

--- duofit/duofit/settings.py
+++ duofit/duofit/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['toniiglesiasc.pythonanywhere.com']
+ALLOWED_HOSTS = ['toniiglesiasc.pythonanywhere.com', '127.0.0.1', 'localhost']
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
